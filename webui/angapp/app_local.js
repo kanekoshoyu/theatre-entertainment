6487fdb6df709819821bed8d07a71fea4bfda022
kanekoshoyu <kanekoshoyu@gmail.com>
2018-02-08 18:01:33 +0800

    add

---
+++ webui/angapp/app_local.js
@@ -0,0 +1,14 @@
+// API connection settings
+app.constant('API_URL', {
+  'url': 'https://algoboro.atlas.thrinacia.com/api',
+  'loc': '/service/restv1/'
+});
+// Localization settings
+app.constant('LANG', {
+  'PREFERRED_LANG' : 'en',
+  'DEFAULT_LANG' : 'en'
+});
+// CDN settings
+app.constant('CDN', {
+  'APP_URL' : 'https://cdn5.thrinacia.com/**'
+});
